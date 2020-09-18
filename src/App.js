899629d742832c140a49a220ef5ffb8ed8352eca
Brian Kubes <briankubes@gmail.com>
2020-09-18 09:10:30 -0700

    Switch is working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Form from "./components/Form";
 import Pizza from "./components/Pizza";
@@ -12,15 +12,17 @@ const App = () => {
       <Link to="/order">Order</Link>
 
       <p>You can remove this code and create your own header</p>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/order">
-        <Form />
-      </Route>
-      <Route path="/order/pizza">
-        <Pizza />
-      </Route>
+      <Switch>
+        <Route path="/order/pizza">
+          <Pizza />
+        </Route>
+        <Route path="/order">
+          <Form />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 };
